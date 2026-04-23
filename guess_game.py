f814746cbcba0860f9471444e72c55837048965b
NAPO-cyber <gopalverma2503@gmail.com>
2026-04-24 01:19:53 +0530

    guess game function created

--- guess_game.py
+++ guess_game.py
@@ -1,26 +1,24 @@
-# # guess a number between a range of numbers
+# guess a number between a range of numbers
 import random
 
-print('NUMBER GUESSING GAME...')
+def guess_func(start, end):
+    print('NUMBER GUESSING GAME...')
+    num = random.randint(start, end)
+    
+    chance = 7 # total chances
+    count = 0 # counter for the guess
+    
+    while count < chance:
+        count += 1
+        guess = int(input("enter your guess: "))
 
-start = int(input('enter starting number: '))
-end = int(input('enter ending number: '))
-num = random.randint(start, end)
+        if guess == num:
+            print("congrats! correct guess...")
+        elif count >= chance and guess != num:
+            print('better luck next time...')
+        elif guess > num:
+            print('guess is too high')
+        elif guess < num:
+            print('guess is too low')
 
-chance = 7 # total chances
-count = 0 # counter for the guess
-
-while count < chance:
-    count += 1
-    guess = int(input("enter your guess: "))
-
-    if guess == num:
-        print("congrats! correct guess...")
-    elif count >= chance and guess != num:
-        print('better luck next time...')
-    elif guess > num:
-        print('guess is too high')
-    elif guess < num:
-        print('guess is too low')
-
-        
+guess_func(2, 10)
